a9c0d23f6965d203e154bc8dc4770dd2e5cd77b9
Brian Campbell <brian.k.campbell@outlook.com>
2022-12-29 01:00:46 -0500

    Day 17

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,22 +1,139 @@
-private const val FILENAME: String = "day14-sample.txt"
-//private const val FILENAME: String = "day14.txt"
+import java.lang.Integer.min
+import java.lang.Integer.max
 
-private const val HEIGHT: Int = 500
+//private const val FILENAME: String = "day14-sample.txt"
+private const val FILENAME: String = "day14.txt"
+
+private const val ORIGIN_X: Int = 500
+private const val ORIGIN_Y: Int = 0
+
+private const val ROCK: Char = '#'
+private const val SAND: Char = 'o'
+private const val SPACE: Char = '.'
+
 fun main() {
-    part01()
-    part02()
-}
+    val originalMap: MutableMap<Int, MutableMap<Int, Char>> = createMap()
+    var part2Map: MutableMap<Int, MutableMap<Int, Char>> = copyMap(originalMap)
 
-private fun part01() {
     println("Part 1:\n")
-    val input = Common.readFile(FILENAME)
+    var grains: Int = 0
+    while (dropSand(originalMap, 500, 0, mutableMapOf<Int, Char>())) {
+        grains += 1
+    }
+    println("$grains grains of sand dropped before abyss\n")
 
-    println("\n")
+    println("Part 2:\n")
+    val mapFloor = findFloorToMap(part2Map)
+    part2Map.forEach { (_, t) -> t[mapFloor] = ROCK }
+    grains = 0
+    while (part2Map[ORIGIN_X]?.get(ORIGIN_Y) != SAND) {
+        dropSand(part2Map, 500, 0, getFloorColumn(mapFloor))
+        grains += 1
+    }
+    println("$grains grains of sand dropped before blockage\n")
 }
 
-private fun part02() {
-    println("Part 2:\n")
-    val input = Common.readFile(FILENAME)
+private fun dropSand(map: MutableMap<Int, MutableMap<Int, Char>>, prevX: Int, prevY: Int,
+                     defaultColumn: MutableMap<Int, Char>): Boolean {
+    var x: Int = prevX
+    var y: Int = prevY
+    // move down
+    var currentColumn: MutableMap<Int, Char> = map.getOrDefault(x, defaultColumn.toMutableMap())
+    if (currentColumn.isEmpty()) {
+        return false
+    } else {
+        val filledSpacesBelow = currentColumn.keys.filter { it > y }
+        if (filledSpacesBelow.isEmpty()) {
+            return false
+        } else {
+            y = filledSpacesBelow.min() - 1
+            map[x] = currentColumn
+        }
+    }
+    // move down-left diagonally
+    var colToLeft: MutableMap<Int, Char> = map.getOrDefault(x - 1, defaultColumn.toMutableMap())
+    if (colToLeft.isEmpty()) {
+        return false
+    } else {
+        var destination = colToLeft.getOrDefault(y + 1, SPACE)
+        if (destination == SPACE) {
+            x -= 1
+            y += 1
+        } else {        // move down-right diagonally
+            var colToRight: MutableMap<Int, Char> = map.getOrDefault(x + 1, defaultColumn.toMutableMap())
+            if (colToRight.isEmpty()) {
+                return false
+            } else {
+                destination = colToRight.getOrDefault(y + 1, SPACE)
+                if (destination == SPACE) {
+                    x += 1
+                    y += 1
+                } else {
+                    currentColumn[y] = SAND
+                    return true
+                }
+            }
+        }
+    }
 
-    println("\n")
+    return dropSand(map, x, y, defaultColumn)
+}
+
+private fun getFloorColumn(floor: Int): MutableMap<Int, Char> {
+    var floorColumn: MutableMap<Int, Char> = mutableMapOf<Int, Char>()
+    floorColumn[floor] = ROCK
+
+    return floorColumn
+}
+
+private fun copyMap(map: MutableMap<Int, MutableMap<Int, Char>>): MutableMap<Int, MutableMap<Int, Char>> {
+    var newMap: MutableMap<Int, MutableMap<Int, Char>> = mutableMapOf<Int, MutableMap<Int, Char>>()
+
+    map.forEach { (x, yColumn) ->
+        newMap[x] = yColumn.toMutableMap()
+    }
+
+    return newMap
+}
+
+private fun createMap(): MutableMap<Int, MutableMap<Int, Char>> {
+    val input = Common.readFile(FILENAME)
+    val map: MutableMap<Int, MutableMap<Int, Char>> = mutableMapOf<Int, MutableMap<Int, Char>>()
+
+    input.forEach { path ->
+        val pathPoints: List<List<Int>> = path.split(" -> ").map {
+                point -> point.split(",").map(String::toInt)
+        }
+        for (i: Int in 0..pathPoints.size - 2) {
+            val x1 = pathPoints[i][0]
+            val y1 = pathPoints[i][1]
+            val x2 = pathPoints[i + 1][0]
+            val y2 = pathPoints[i + 1][1]
+
+            if (x1 == x2) { // vertical line
+                val yRocks = map.getOrDefault(x1, mutableMapOf<Int, Char>())
+                val lowY = min(y1, y2)
+                val highY = max(y1, y2)
+                for (y: Int in lowY..highY) {
+                    yRocks[y] = ROCK
+                }
+                map[x1] = yRocks
+            } else {        // horizontal line
+                val lowX = min(x1, x2)
+                val highX = max(x1, x2)
+                for (x: Int in lowX .. highX) {
+                    val yRocks = map.getOrDefault(x, mutableMapOf<Int, Char>())
+                    yRocks[y1] = ROCK
+                    map[x] = yRocks
+                }
+            }
+        }
+    }
+    return map
+}
+
+private fun findFloorToMap(map: MutableMap<Int, MutableMap<Int, Char>>): Int {
+    return map.values.map {
+        it.keys.max()
+    }.max() + 2
 }
--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,21 +1,169 @@
-private const val FILENAME: String = "day-sample.txt"
-//private const val FILENAME: String = "day.txt"
+private const val FILENAME: String = "day17-sample.txt"
+//private const val FILENAME: String = "day17.txt"
+
+private const val SHAFT_WIDTH: Int = 7
+private const val ROCK: Char = '#'
+private const val SPACE: Char = '.'
+private const val LEFT: Char = '<'
 
 fun main() {
-    part01()
-    part02()
-}
-
-private fun part01() {
     println("Part 1:\n")
-    val input = Common.readFile(FILENAME)
+    val windMovement = Common.readFile(FILENAME)[0]
 
-    println("\n")
+    var windIndex: Int = 0
+    val shapes = listOf(::HLine, ::Cross, ::LShape, ::VLine, ::Box)
+    var shapeIndex: Int = 0
+    val lastShapeIndex: Int = 2022
+    val shaft: MutableList<String> = mutableListOf<String>(ROCK.toString().repeat(SHAFT_WIDTH))
+
+    for (i in 1..lastShapeIndex) {
+        val highestRock: Int = List(shaft.filterIndexed { _, s -> s.contains(ROCK) }.size) { index -> index }.max()
+
+        // add space for shape to fall
+        for (j in shaft.size..highestRock + 4) {
+            shaft.add(SPACE.toString().repeat(SHAFT_WIDTH))
+        }
+
+        // create shape
+        val currentShape: RockShape = shapes[shapeIndex]()
+        currentShape.x = 2
+        currentShape.y = highestRock + 4
+
+        if (windMovement[windIndex] == LEFT) {
+            currentShape.moveLeft(shaft)
+        } else {
+            currentShape.moveRight(shaft)
+        }
+        windIndex = (windIndex + 1).mod(windMovement.length)
+
+        while(currentShape.drop(shaft)) {
+            if (windMovement[windIndex] == LEFT) {
+                currentShape.moveLeft(shaft)
+            } else {
+                currentShape.moveRight(shaft)
+            }
+            windIndex = (windIndex + 1).mod(windMovement.length)
+        }
+
+        placeRock(shaft, currentShape)
+
+        shapeIndex = (shapeIndex + 1).mod(shapes.size)
+
+        if (shapeIndex == 0) {
+            val shaftHeight = List(shaft.filterIndexed { _, s -> s.contains(ROCK) }.size) { index -> index }.max()
+
+            println("Height: $shaftHeight, Stone $i, windIndex: $windIndex")
+        }
+    }
+
+    println("Tower Height: ${ List(shaft.filterIndexed { _, s -> s.contains(ROCK) }.size) { index -> index }.max() }\n")
+
+    /*
+    00 / 24 / 12 / 02 / 28 / 15 / 05 / 34 / 21 / 10 /
+                   02 / 28 / 15 / 05 / 34 / 21 / 10 /
+                   02 -> ..####. 25  78 131 - 15
+                   28 -> .####.. 36  89 142 - 20
+                   15 -> ..####. 43  96 149 - 25
+                   05 -> #...... 51 104 157 - 30
+                   34 -> .####.. 60 113 166 - 35
+                   21 -> ..####. 66 119 172 - 40
+                   10 -> ..####. 72 125 178 - 45
+
+                   1,000,000,000,000 - 15 =
+                   999,999,999,985 / (50 - 15) =
+                   999,999,999,985 / 35 =
+                   28,571,428,571 * 53 =
+                   1,514,285,714,263 + 25 =
+                   1,514,285,714,288
+
+                   1,000,000,000,000 - 65 =
+                   999,999,999,935 / (1775 - 65) =
+                   999,999,999,935 / (1710) =
+                   584,795,321 + remainder
+                   584,795,321 * 1710 = 999,999,998,910
+                   999,999,999,935 - 999,999,998,910 = 1025
+
+                   107 + ((2754 - 107) * 584,795,321) =
+                   107 + (2647 * 584,795,321) = 1,547,953,214,794
+                   1,547,953,214,794 + (1706 - 107) =
+                   1,547,953,214,794 + 1599 =
+                   1,547,953,216,393
+
+
+     */
 }
 
-private fun part02() {
-    println("Part 2:\n")
-    val input = Common.readFile(FILENAME)
 
-    println("\n")
+private fun placeRock(shaft: MutableList<String>, rock: RockShape) {
+    rock.massLocations.forEach {location ->
+        val x = rock.x + location[0]
+        val y = rock.y + location[1]
+        shaft[y] = shaft[y].substring(0, x) + ROCK + shaft[y].substring(x + 1)
+    }
 }
+
+private interface IRockShape {
+    var x: Int
+    var y: Int
+    fun moveRight(shaft: MutableList<String>)
+    fun moveLeft(shaft: MutableList<String>)
+    fun drop(shaft: MutableList<String>) : Boolean
+}
+
+private open class RockShape(val massLocations: List<List<Int>>,
+                             val width: Int,
+                             val height: Int): IRockShape {
+
+    override var x: Int = Int.MAX_VALUE
+    override var y: Int = Int.MAX_VALUE
+
+    override fun moveRight(shaft: MutableList<String>) {
+        massLocations.forEach {location ->
+            val newX = x + location[0] + 1
+            val yLocation = y + location[1]
+            if (newX == SHAFT_WIDTH || (yLocation < shaft.size && shaft[yLocation][newX] == ROCK)) {
+                return
+            }
+        }
+        x += 1
+    }
+
+    override fun moveLeft(shaft: MutableList<String>) {
+        massLocations.forEach {location ->
+            val newX = x + location[0] - 1
+            val yLocation = y + location[1]
+            if (newX < 0 || (yLocation < shaft.size && shaft[yLocation][newX] == ROCK)) {
+                return
+            }
+        }
+        x -= 1
+    }
+
+    override fun drop(shaft: MutableList<String>) : Boolean {
+        massLocations.forEach {location ->
+            val newY = y + location[1] - 1
+            val xLocation = x + location[0]
+            if (newY < shaft.size && shaft[newY][xLocation] == ROCK) {
+                return false
+            }
+        }
+        y -= 1
+        return true
+    }
+}
+
+private class HLine() : RockShape(listOf(listOf(0, 0), listOf(1, 0), listOf(2, 0), listOf(3, 0)), 4, 1) {
+}
+
+private class Cross() : RockShape(listOf(listOf(1, 0), listOf(0, 1), listOf(1, 1), listOf(2, 1), listOf(1, 2)), 3 ,3) {
+}
+
+private class LShape() : RockShape(listOf(listOf(0, 0), listOf(1, 0), listOf(2, 0), listOf(2, 1), listOf(2, 2)), 3, 3) {
+}
+
+private class VLine() : RockShape(listOf(listOf(0, 0), listOf(0, 1), listOf(0, 2), listOf(0, 3)), 1, 4) {
+}
+
+private class Box() : RockShape(listOf(listOf(0, 0), listOf(1, 0), listOf(0, 1), listOf(1, 1)), 2, 2) {
+}
+
